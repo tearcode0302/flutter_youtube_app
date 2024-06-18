25b3209471bb845c613175c5906a1644a8d6ab14
sangjun <byjun0302@gmail.com>
2024-06-18 15:14:07 +0900

    feat: add HomeScreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_youtube_app/main.dart';
+import 'package:flutter_youtube_app/screen/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(HomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
